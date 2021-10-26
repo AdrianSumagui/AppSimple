beaea58dcc34e46e8c2ef4337c4d32b4d6ab96c7
AdrianDEV <adriansumagui@outlook.com>
2021-10-26 15:19:40 +0200

    Configurar las rutas de la API para conectarse con la Base de Datos a través del puerto 8080

--- server.js
+++ server.js
@@ -8,5 +8,25 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// HTTP request logger.
+
 app.use(morgan('tiny'));
+
+// Rutas
+
+app.get('/api', (req, res) => {
+
+    const data = {
+
+        todo: 'Jugar a Valorant',
+        todoDescription: 'Jugar con los panas'
+
+    };
+
+    res.json(data);
+
+
+}) ;
+
+app.listen(PORT, console.log(`Servidor desplegado en el puerto ${PORT}`));
 
