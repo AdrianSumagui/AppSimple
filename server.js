59851a59ea4cb4b72afb2038402c2e8604846e8f
AdrianDEV <adriansumagui@outlook.com>
2021-10-28 13:28:34 +0200

    Creo el modelo y Schema de Todo y me conecto a la base de datos y guardo ahí los datos.

--- server.js
+++ server.js
@@ -8,6 +8,64 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+mongoose.connect('mongodb://localhost/AppSimple', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+});
+
+// Verifico que mongoose se ha conectado con éxito.
+
+mongoose.connection.on('connected', () => {
+
+    console.log('Mongoose está conectado.')
+
+});
+
+// Schema
+
+const Schema = mongoose.Schema;
+const todoSchema = new Schema({
+
+    todoTitle: String,
+    todoDescription: String
+
+});
+
+// Modelo
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+// Guardar datos a la base de datos.
+
+const data = {
+
+    todoTitle: 'Tarde de Valos',
+    todoDescription: 'Jugar con los Panas al Valorant'
+
+};
+
+// Instanciar el modelo. 
+
+const newTodo = new Todo(data);
+
+newTodo.save((error) => {
+
+    if (error) {
+
+        console.log('Ha ocurrido un error. :(')
+
+    } else {
+
+        console.log('Los datos se han guardado correctamente. :D')
+
+    }
+
+});
+
+// .save();
+
 // HTTP request logger.
 
 app.use(morgan('tiny'));
@@ -18,7 +76,7 @@ app.get('/api', (req, res) => {
 
     const data = {
 
-        todo: 'Jugar a Valorant',
+        todoTitle: 'Jugar a Valorant',
         todoDescription: 'Jugar con los panitas'
 
     };
